55747c7541c934fdd84a0905d014c59b07a90fe9
Dener-arx <54905778+Dener-arx@users.noreply.github.com>
2022-12-02 13:26:17 -0300

    Update producer.py

--- producer.py
+++ producer.py
@@ -2,16 +2,13 @@ from kafka import KafkaProducer
 from const import *
 import sys
 
-try:
-    topic = sys.argv[1]
-except:
-    print ('Usage: python3 producer <topic_name>')
-    exit(1)
-    
+topic = ['Health', 'Sports', 'Television']
+
 producer = KafkaProducer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
-for i in range(100):
-    msg = 'My ' + str(i) + 'st message for topic ' + topic
-    print ('Sending message: ' + msg)
+
+for topic in topics:
+    msg = topic + ' related message'
+    print('Sending message about ' + topic + ': ' + msg)
     producer.send(topic, value=msg.encode())
 
 producer.flush()
