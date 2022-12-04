211dddf9742d67fc2d2a47f4a93d94548e7d4894
felipelagares <62161177+felipelagares@users.noreply.github.com>
2022-12-04 09:45:55 -0300

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -4,10 +4,17 @@ import sys
 
 consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT],
                         auto_offset_reset='earliest',
-                        enable_auto_commit='false');
+                        enable_auto_commit='false')
 
+topics_to_subscribe = []
+print('type "none" to start follow your topics')
+while True:
+    topico = input('new topic:')
+    if topico == 'none':
+        break
+    topics_to_subscribe.append(topico)
 
-consumer.subscribe(['Health'])
+consumer.subscribe(topics_to_subscribe)
 
 for msg in consumer:
-    print (msg.value)
+    print(msg.value)
