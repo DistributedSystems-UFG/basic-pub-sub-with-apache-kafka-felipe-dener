df1823ad75c8229d6763cc0433a43a049e323f9e
felipelagares <62161177+felipelagares@users.noreply.github.com>
2022-12-04 09:29:26 -0300

    Update producer.py

--- producer.py
+++ producer.py
@@ -1,14 +1,10 @@
 from kafka import KafkaProducer
 from const import *
-import sys
-
-topics = ['Health', 'Sports', 'Television']
 
 producer = KafkaProducer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
 
-for topic in topics:
-    msg = topic + ' related message'
-    print('Sending message about ' + topic + ': ' + msg)
+while True:
+    topic = input("topico: ")
+    msg = input("msg: ")
     producer.send(topic, value=msg.encode())
-
-producer.flush()
+    producer.flush()
